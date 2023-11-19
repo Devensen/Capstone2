a5ae7ce1943b62b00bcfecdb5de319f2ea812c82
Rick Evensen <evensenrick@gmail.com>
2023-11-19 11:57:05 -0500

    h1 added to page

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let btn = document.getElementById('btn');
 let output = document.getElementById('output');
 
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', getQuote);
   const api_url ="https://api.quotable.io/quotes/random";
 
   async function getapi(url)
@@ -11,10 +11,42 @@ btn.addEventListener('click', function(){
     var data = await response.json();
     console.log(data);
   }
-  
-  getapi(api_url);
 
-})
+  getapi(api_url);
+  
+
+  // below is for the JS code for displaying the countdown to New Years
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+   
+
+
+
+
 
 
 
