b6e0bccbfc5dd82aafa49636fa18aa202b28df08
Rick Evensen <evensenrick@gmail.com>
2023-11-22 20:56:56 -0500

    API displaying data on page and fixed CSS

--- main.js
+++ main.js
@@ -1,52 +1,36 @@
-let btn = document.getElementById('btn');
-let output = document.getElementById('output');
+const btn = document.getElementById('btn');
+const output = document.getElementById('output');
 
+const countdownContainer = document.getElementById("countdown");
+
+async function getQuote()
+{
+  const url ="https://api.quotable.io/quotes/random";
+  const response = await fetch(url);
+  let data = await response.json();
+
+  const quoteOutput = document.getElementById('output');
+  quoteOutput.innerHTML = `"${data[0].content}" - ${data[0].author}`;
+  quoteOutput.style.display = 'flex';
+}
+
+function updateCountdown() {
+  
+  let newYear = new Date("January 1, 2024 00:00:00");
+  let currentTime = new Date();
+
+  let diff = (newYear - currentTime);
+
+  let days = Math.floor(diff / (1000 * 60 * 60 * 24))
+  let hours = Math.floor((diff % (1000 + 60 * 60 * 24)) / (1000 * 60 * 60))
+  let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))
+  let seconds = Math.floor((diff % (1000 * 60)) / 1000)
+
+  console.log(`${days} : ${hours} : ${minutes} : ${seconds}`)
+
+  countdownContainer.innerHTML = `<p>${days} : ${hours} : ${minutes} : ${seconds}</p>`;
+}
 
 btn.addEventListener('click', getQuote);
-  const api_url ="https://api.quotable.io/quotes/random";
 
-  async function getapi(url)
-  {
-    const response = await fetch(url);
-    var data = await response.json();
-    console.log(data);
-  }
-
-  getapi(api_url);
-  
-
-  // below is for the JS code for displaying the countdown to New Years
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
- 
-   
-
-
-
-
-
-
-
+setInterval(updateCountdown, 1000);
